70c5b3fbebcebdaf5815c8018461ac29aab9f339
Vladislav N <vladislav.n@ruport.ru>
2023-05-09 15:22:05 +0300

    initial

---
+++ pages/products.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Layout from '@/components/Layout';
+import Link from 'next/link';
+
+export default function Products() {
+  return (
+    <Layout>
+      <Link href='/products/new' className="bg-blue-900 rounded-md text-white py-1 px-2 ">Add new product</Link>
+    </Layout>
+  )
+}
---
+++ pages/products/new.js
@@ -0,0 +1,10 @@
+import Layout from '@/components/Layout'
+import React from 'react'
+
+export default function NewProduct() {
+  return (
+    <Layout>
+      <input type="text" placeholder=''></input>
+    </Layout>
+  )
+}
